32ad0fd050397ef195d88a1c58f0eba9b8da519f
shravan <shravankumarc87@gmail.com>
2025-03-01 22:52:20 +0530

    first commit

---
+++ src/test/java/GFG.java
@@ -0,0 +1,31 @@
+public class GFG {
+    // Java Program to demonstrate Brute Force Method to check if a number is prime
+        static boolean isPrime(int n)
+        {
+            // Corner case
+            if (n <= 1)
+                return false;
+
+            // Check from 2 to n-1
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        // Driver Program
+        public static void main(String args[])
+        {
+            if (isPrime(11))
+                System.out.println(" true");
+            else
+                System.out.println(" false");
+            if (isPrime(15))
+                System.out.println(" true");
+            else
+                System.out.println(" false");
+        }
+
+
+}
